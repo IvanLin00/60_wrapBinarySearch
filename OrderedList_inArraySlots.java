618a1189326cc4b91b1aae9eebb20bd8a5ef7fe6
IvanLin00 <47545122+IvanLin00@users.noreply.github.com>
2019-04-11 21:48:35 -0400

    added cost

--- OrderedList_inArraySlots.java
+++ OrderedList_inArraySlots.java
@@ -1,147 +1,71 @@
 /**
-  OrderedList_inArraySlots, holding Integers
-
-  A list with the invariant the elements are in order by
-  increasing value, implemented using a
-  ArrayList<Integer>
+  test indexOf in OrderedList_inArraySlots
  */
 
-public class OrderedList_inArraySlots
-    implements OrderedList {
+public class UserOfBinarySearch {
+    public static OrderedList_inArraySlots orderedList =
+      new OrderedList_inArraySlots();
 
-    private java.util.ArrayList<Integer> list_iAS;
-
-
-    /**
-      @return the index of any occurrence of
-              \findMe in this list, or -1 if
-              \findMe is absent from this list.
-     */
-    public int indexOf( Integer findMe) {
-        return indexOf_whileStyle( findMe);
-        // return indexOf_recursive(
-            // findMe, 0, list_iAS.size() -1);
-    }
-
-
-    /**
-      @return the indexOf value, calculated while-style
-     */
-    private int indexOf_whileStyle( Integer findMe) {
-        int low = 0;
-        int hi  = list_iAS.size() -1;  // inclusive
-
-        while( low <= hi){
-            int pageToCheck = (low + hi) / 2;
-            int comparison =
-              findMe.compareTo( list_iAS.get( pageToCheck));
-            if( comparison == 0) return pageToCheck;
-            else
-                if( comparison < 0)
-                    // findMe's spot precedes pageToCheck
-                    hi = pageToCheck -1;
-                // findMe's spot follows pageToCheck
-                else low = pageToCheck +1;
+    public static void main(String[] commandLine) {
+        System.out.println();
+        
+        /* create a list whose values are easy to
+           distinguish from indexes */
+        Integer valNotIndex = 111;
+        for( int count = 0; count < 10; count++){
+            orderedList.add( valNotIndex);
+            valNotIndex += 2;  // to leave gaps
         }
-        return -3; // value differs from skeleton, just FYI
-    }
+        System.out.println( "sample list: " + orderedList
+                          + System.lineSeparator());
 
-
-    /**
-      @return the indexOf value, calculated recursively
-      [Holmes's comments temporarily elided, so as
-       to avoid spoilers for hw60_16]
-     */
-    private int indexOf_recursive( Integer findMe
-                                 , int low
-                                 , int hi // inclusive
-                                 ) {
-        // System.out.println( "debug low: " + low
-                          // + "   hi: " + hi);
-        if( low > hi)  // detect base case
-            return -2;   // solution to base case
-              // value differs from while-style method, just FYI
-        else{
-            int pageToCheck = (low + hi) / 2;
-            int comparison =
-              findMe.compareTo( list_iAS.get( pageToCheck));
-
-
-            if( comparison == 0)    // detect base case
-                return pageToCheck; // solution other base case
-            // recursive cases
-            else
-                if( comparison < 0)
-                    // findMe's spot precedes pageToCheck
-                    return indexOf_recursive( findMe
-                                             , low
-                                             , pageToCheck -1);
-                else
-                    // findMe's spot follows pageToCheck
-                    return indexOf_recursive( findMe
-                                            , pageToCheck +1
-                                            , hi);
+        System.out.println( "search for values THAT EXIST");
+        Integer findMe;  // value to find
+        int foundAt;     // index where value is found
+        System.out.println();
+        for( int i = 0; i < orderedList.size(); i++) {
+			int expectedCost = 0;
+			if (i==4) expectedCost=1;
+			if (i==1||i==7) expectedCost = 2;
+			if (i ==0|| i==2 || i== 5||i==8) expectedCost = 3;
+			if (i==3||i==6||i==9) expectedCost = 4;
+			
+            findMe = orderedList.get( i);
+            foundAt = orderedList.indexOf( findMe);
+            System.out.println( 
+                "Value " + findMe
+              + " was found at index " + foundAt
+              + ". That's correct, eh? " + (foundAt == i)
+			  + "cost correct?" + (orderedList.getCost() == expectedCost)
+              );
         }
-    }
+        System.out.println();
 
 
-    // ------ code from previous assignments below here ----
-
-    public OrderedList_inArraySlots() {
-        list_iAS = new java.util.ArrayList<Integer>();
-    }
-
-    /**
-      @return the number of elements in this list
-     */
-    public int size(){
-        return list_iAS.size();
-    }
-
-    /**
-      @return a string representation of this Orderedlist_iAS
-     */
-    public String toString() {
-        return list_iAS.toString();
+        System.out.println( "search for MISSING values");
+        findOne( "not in middle"
+               , orderedList.get( orderedList.size() /2) +1);
+		System.out.println("cost correct: " + (orderedList.getCost() == 4));
+        findOne( "before first ", orderedList.get(0) -5);
+		System.out.println("cost correct: " + (orderedList.getCost() == 3));
+        findOne( "after last   "
+               , orderedList.get( orderedList.size() -1) +5);
+		System.out.println("cost correct: " + (orderedList.getCost() == 4));
+        System.out.println();
     }
 
 
     /**
-      Put @value where it belongs in the list.
-      @pre: the list is in increasing order
+      report on indexOf's search for the given value
      */
-     public boolean add( Integer value) {
-         int dest = 0;
-         for( ; dest < list_iAS.size() && list_iAS.get( dest) < value
-              ; dest++) ;
-         // System.out.println( "OL adding " + value
-         //                   + " at index " + dest);
-         list_iAS.add( dest, value);
-         return true;
+    private static void findOne( String description
+                               , Integer value
+                              ) {
+        int foundAt = orderedList.indexOf( value);
+        System.out.println( description + ": "
+          + " value " + value
+          + " \"found\" at index " + foundAt
+          + ". That's correct, eh? " + (foundAt < 0)
+          );
      }
-
-
-     /**
-      @return element @index from this list
-      precondition: @index is within the bounds of the array.
-          (Having warned the user about this precondition,
-           you should NOT complicate your code to check
-           whether the condition was violated.)
-     */
-    public Integer get( int index ) {
-        return list_iAS.get( index);
-    }
-
-
-    /**
-      Remove the element at position @index in this list.
-
-      Shift any subsequent elements to the left (that is,
-      decrease the index associated with each).
-
-      @return the value that was removed from the list
-     */
-    public Integer remove( int index) {
-        return list_iAS.remove( index);
-    }
 }
